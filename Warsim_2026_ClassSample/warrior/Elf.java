3b5fa920ac1150792b071eb36b809aa25914dbdd
Lam Nguyen <lam.nguyen82@gmail.com>
2026-04-07 16:36:00 -0400

    feat: implement randomized base stats for health, strength, and dexterity in Elf class

--- Warsim_2026_ClassSample/warrior/Elf.java
+++ Warsim_2026_ClassSample/warrior/Elf.java
@@ -1,1 +1,18 @@
+
 package warrior;
+
+  public class Elf extends Warrior {
+    private int minHealth = 180;
+    private int bonusHealth = 220;  //  health range of 180 - 399
+    private int minStrength = 75;
+    private int bonusStrength = 120;
+    private int minDexterity = 300;
+    private int bonusDexterity = 300;
+
+    public Elf() {
+      super();
+      super.setHealth(randNum.nextInt(this.bonusHealth) + this.minHealth);
+      super.setStrength(randNum.nextInt(this.bonusStrength) + this.minStrength);
+      super.setDexterity(randNum.nextInt(this.bonusDexterity) + this.minDexterity);
+    }   // constructor
+  } // End Class
